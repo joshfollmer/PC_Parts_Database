18ca23cb8caa4003891a45b64b8aae3dd2355f21
joshfollmer <josh.follmer@icloud.com>
2023-06-13 09:39:47 -0700

    got chromedirver working

--- CPU_inserter.py
+++ CPU_inserter.py
@@ -3,6 +3,8 @@ import configparser as cf
 from pcpartpicker import API
 import itertools
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
 def connectToDB():
     config = cf.ConfigParser()
@@ -28,13 +30,26 @@ def connectToDB():
 # cpu_data = api.retrieve("cpu")
 # values = cpu_data.values()
 
-driver = webdriver.Chrome()
-driver.get("https://www.techpowerup.com/cpu-specs/?sort=name")
+# Set Brave-specific options
+options = webdriver.ChromeOptions()
+options.binary_location = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe'
 
-td_elements = driver.find_element_by_css_selector("td")
+# Create the ChromeDriver instance for Brave
+driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install(), options=options)
 
-for td in td_elements:
-    print(td.text)
+# Open a website
+driver.get('https://www.example.com')
+
+# Find all <p> elements
+p_elements = driver.find_elements_by_tag_name('p')
+
+# Print the text of each <p> element
+print('All <p> tags:')
+for element in p_elements:
+    print(element.text)
+# Quit the browser
+driver.quit()
+
 # flattened_values = itertools.chain(*values)
 # #relevant attributes: base_clock, boost_clock, brand, cores, integrated graphics, model, multithreading, price, tdp
 # for element in flattened_values:
