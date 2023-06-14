362fa8cc7a640a779bcafe82b513cdf46d1480aa
joshfollmer <josh.follmer@icloud.com>
2023-06-14 00:21:32 -0700

    Update CPU_inserter.py

--- CPU_inserter.py
+++ CPU_inserter.py
@@ -27,44 +27,57 @@ def connectToDB():
     return conn.cursor()
 
 
-# api = API()
-# cpu_data = api.retrieve("cpu")
-# values = cpu_data.values()
 
-# Set Brave-specific options
+
+# # Set Brave-specific options
 options = webdriver.ChromeOptions()
 options.binary_location = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe'
 
-# Create the ChromeDriver instance for Brave
+# # Create the ChromeDriver instance for Brave
 driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install(), options=options)
 
 # Open a website
 driver.get('https://www.techpowerup.com/cpu-specs/')
-#wait = driver.WebDriverWait(driver, 10)
-
-
+# find the search bar
 search_bar = driver.find_element_by_id("quicksearch")
-search_bar.send_keys("Core i7-13700TE")
-sleep(5)
 
-# Find all <tr> elements
-td_elements = driver.find_elements_by_tag_name('tr')
 
-# Print the text of each <tr> element
-print('All <td> tags:')
-for element in td_elements:
-    print(element.text)
+#open the pcpartpicker API
+api = API()
+cpu_data = api.retrieve("cpu")
+values = cpu_data.values()
+#put the pcpart object into a more readable format
+flattened_values = itertools.chain(*values)
+
+#relevant attributes: base_clock, boost_clock, brand, cores, integrated graphics, model, multithreading, price, tdp
+#for 10 times, loop through the API, put the model name into the searchbar, and find the result we are looking for 
+counter = 0
+for element in flattened_values:
+    if(counter > 10):
+        break
+
+    print(element.model)
+    search_bar.send_keys(element.model)
+    sleep(5)
+    table = driver.find_element_by_css_selector("table.processors")
     
-# Quit the browser
-driver.quit()
+    tr_elements = driver.find_elements_by_tag_name('tr')
+    for result in tr_elements:
+        print(result.text)
+        # result_name = str(result)
+        # result_name = result_name.split("/", 1)
+        # result_name = result_name[0].strip()
+        # if(result_name == element.model):
+        #     print("hooray!")
+        # else:
+        #     print(f"booo! result name: {result_name}, element model: {element.model}")
+            
 
-# flattened_values = itertools.chain(*values)
-# #relevant attributes: base_clock, boost_clock, brand, cores, integrated graphics, model, multithreading, price, tdp
-# for element in flattened_values:
-#     print(element)
+    counter += 1
+    sleep(1)
    
 
-
-    
+# Quit the browser
+driver.quit()
 
     
