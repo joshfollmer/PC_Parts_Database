9bc280ad159a4e0b994c2c19edd37084ce0b5755
joshfollmer <josh.follmer@icloud.com>
2023-06-26 22:06:07 -0700

    changing approach

--- SSD_inserter.py
+++ SSD_inserter.py
@@ -1,11 +1,9 @@
-from pcpartpicker import API
-import itertools
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-
 
 
 
@@ -19,33 +17,17 @@ driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.BRAVE).inst
 driver.get('https://www.techpowerup.com/ssd-specs/')
 # find the search bar
 search_bar = driver.find_element_by_css_selector(".js-search-input.search-input")
+#!new approach: type two letters into search bar (aa, ab, ac, etc) and then visit each entry and insert based on info from page
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-api = API()
-#relevant attributes: brand, cache_amount, capacity, form_factor, interface, model, storage_type
-cpu_data = api.retrieve("internal-hard-drive")
-values = cpu_data.values()
-#put the pcpart object into a more readable format
-flattened_values = itertools.chain(*values)
 
 try:
-    counter = 0
-    for element in flattened_values:
-        if(counter <= 10 and element.storage_type == "SSD"):
-            search_bar.send_keys(element.model)
+    for i in range(len(alphabet)):
+        for j in range(len(alphabet)):
+            search_bar.send_keys(f"{alphabet[i]}{alphabet[j]}")
             sleep(5)
 
-            ssd_table = driver.find_elements_by_css_selector("table.drives-desktop-table")
 
-            for item in ssd_table:
-                a_elements = item.find_elements_by_tag_name('a')
-
-                for result in a_elements:
-                    if(result.text.strip()):
-                        link = result.get_attribute("href")
-                        driver.get(link)
-                        sleep(5)
-
-        search_bar.clear()
-        counter += 1
+            
 finally:
     driver.quit()
