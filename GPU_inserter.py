f1ef989c7c77324422f11599dd9d82b9cc8ac121
joshfollmer <josh.follmer@icloud.com>
2023-08-05 15:56:47 -0700

    updated

--- GPU_inserter.py
+++ GPU_inserter.py
@@ -1,12 +1,10 @@
 import mysql.connector
-from mysql.connector import connect, Error
 import configparser as cf
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.common.keys import Keys
 from time import sleep
 from random import randint
+from selenium.webdriver.common.by import By
+from datetime import datetime
 
 
 #function to check if an entry has already been inserted into the table
@@ -17,21 +15,107 @@ def checkIfExists(cursor, table_name, column_name, value):
     count = result[0]
     return count > 0
 
-
+#target specs: brand, model, clock_speed, vram, bus_width, bandwidth, pcie, tdp, length, width, height,
+#shading_units, memory_type, th_pixel_rate, th_texture_rate, th_FP16, th_FP32, th_FP64, architecture, 
+#process size, release_date, transistors
 def parseGpuPage(url, cursor):
-   url = url
+    driver.get(url)
+    sleep(10)
+
+    brand = "unknown"
+    model = "unknown"
+    clock_speed = "unknown"
+    vram = "unknown"
+    bus_width = "unknown"
+    bandwidth = "unknown"
+    pcie = "unknown"
+    tdp = "unknown"
+    length = "unknown"
+    width = "unknown"
+    height = "unknown"
+    shading_units = "unknown"
+    memory_type = "unknown"
+    th_pixel_rate = "unknown"
+    th_texture_rate = "unknown"
+    th_FP16 = "unknown"
+    th_FP32 = "unknown"
+    th_FP64 = "unknown"
+    architecture = "unknown"
+    process_size = "unknown"
+    release_date = "unknown"
+    transistors = "unknown"
+
+    sections = driver.find_elements(By.CSS_SELECTOR, "section.details")
+    name = driver.find_element(By.CLASS_NAME, "gpudb-name").text
+    name = name.split(" ", 1)
+    brand = name[0]
+    model = name[1]
+    
+    for section in sections:
+        h2_element = section.find_element(By.CSS_SELECTOR, "h2")
+
+        if(h2_element.text == "Graphics Processor"):
+            rows = section.find_elements(By.CLASS_NAME, "clearfix")
+            for row in rows:
+                header = row.find_element(By.CSS_SELECTOR, "dt")
+                if(header.text == "Architecture"):
+                    architecture = row.find_element(By.CSS_SELECTOR, "dd").text
+                elif(header.text == "Process Size"):
+                    process_size = row.find_element(By.CSS_SELECTOR, "dd").text
+                elif(header.text == "Transistors"):
+                    transistors = row.find_element(By.CSS_SELECTOR, "dd").text
+
+        elif(h2_element.text == "Graphics Card"):
+            rows = section.find_elements(By.CLASS_NAME, "clearfix")
+            for row in rows:
+                header = row.find_element(By.CSS_SELECTOR, "dt")
+                if(header.text == "Release Date"):
+                    release_date = row.find_element(By.CSS_SELECTOR, "dd").text
+                elif(header.text == "Bus Interface"):
+                    pcie = row.find_element(By.CSS_SELECTOR, "dd").text
+
+        elif(h2_element.text == "Render Config"):
+            rows = section.find_elements(By.CLASS_NAME, "clearfix")
+            for row in rows:
+                header = row.find_element(By.CSS_SELECTOR, "dt")
+                if(header.text == "Shading Units"):
+                    shading_units = row.find_element(By.CSS_SELECTOR, "dd").text  
+
+        elif(h2_element.text == "Theoretical Performance"):
+            rows = section.find_elements(By.CLASS_NAME, "clearfix")
+            for row in rows:
+                header = row.find_element(By.CSS_SELECTOR, "dt")
+                if(header.text == "Pixel Rate"):
+                    th_pixel_rate = row.find_element(By.CSS_SELECTOR, "dd").text
+                elif(header.text == "Texture Rate"):
+                    th_texture_rate = row.find_element(By.CSS_SELECTOR, "dd").text
+                elif(header.text == "FP16(half)"):
+                    th_FP16 = row.find_element(By.CSS_SELECTOR, "dd").text
+                elif(header.text == "FP32(float)"):
+                    th_FP32 = row.find_element(By.CSS_SELECTOR, "dd").text
+                elif(header.text == "Fp64(double)"):
+                    th_FP64 = row.find_element(By.CSS_SELECTOR, "dd").text
+
+        elif(h2_element.text == "Board Design"):
+            rows = section.find_elements(By.CLASS_NAME, "clearfix")
+            for row in rows:
+                header = row.find_element(By.CSS_SELECTOR, "dt")
+                if(header.text == "Length"):
+                    length = row.find_element(By.CSS_SELECTOR, "dd").text
+    
+    list = [brand, model, architecture, process_size, transistors, release_date, pcie, shading_units,
+            th_pixel_rate, th_texture_rate, th_FP16, th_FP32, th_FP64]
+
+    print(length)
 
 
-options = webdriver.ChromeOptions()
-options.binary_location = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe'
 
-# Create the ChromeDriver instance for Brave
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install(), options=options)
-
+driver = webdriver.Chrome()
 # Open a website
 driver.get('https://www.techpowerup.com/gpu-specs/')
+sleep(1)
 # find the search bar
-search_bar = driver.find_element_by_css_selector(".js-search-input.search-input")
+search_bar = driver.find_element(By.NAME, "q")
 #the approach: type two letters into search bar (aa, ab, ac, etc) and then visit each entry and insert based on info from page
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -56,32 +140,35 @@ while(True):
         value = "temp"
         for i in range(len(alphabet)):
             for j in range(len(alphabet)):
-                if(counter > 0):
+                if(counter > 3):
                     #we need to reload the page and relocate the search bar each time because we will be loading new pages
-                    driver.get('https://www.techpowerup.com/ssd-specs/')
-                    search_bar = driver.find_element_by_css_selector(".js-search-input.search-input")
+                    driver.get('https://www.techpowerup.com/gpu-specs/')
+                    search_bar = driver.find_element(By.NAME, "q")
                     search_bar.send_keys(f"{alphabet[i]}{alphabet[j]}")
                     sleep(5)
-                    amd_cards = driver.find_elements_by_css_selector(".vender-AMD")
-                    nvi_cards = driver.find_elements_by_css_selector(".vender-NVIDIA")
-                    intel_cards = driver.find_elements_by_css_selector(".vender-INTEL")
+                    amd_cards = driver.find_elements(By.CSS_SELECTOR, ".vendor-AMD")
+                    nvi_cards = driver.find_elements(By.CSS_SELECTOR, ".vendor-NVIDIA")
+                    intel_cards = driver.find_elements(By.CSS_SELECTOR, ".vendor-INTEL")
                     cards = amd_cards + nvi_cards + intel_cards
                     link_attributes = []
                     for item in cards:
-                        a_elements = item.find_elements_by_tag_name('a')
+                        a_elements = item.find_elements(By.TAG_NAME, 'a')
                         for result in a_elements:
                             if result.text.strip():
-                                # href = result.get_attribute("href")
-                                # text = result.text
-                                # link_attributes.append({"href": href, "text": text})
-                                print(item.text)
+                                href = result.get_attribute("href")
+                                text = result.text
+                                link_attributes.append({"href": href, "text": text})
+                                
 
-                    # for item in link_attributes:
-                    #     parseGpuPage(item["href"], cursor)
+                    for item in link_attributes:
+                        parseGpuPage(item["href"], cursor)
                 counter += 1
                 print(f"counter is {counter}")
-    except:
-        sleep(300)          
-        
+
+    #except:
+        # now = datetime.now()
+        # print(f"Error at {now.strftime('%H:%M:%S')}. Waiting for 20 minutes")
+        # sleep(1200) 
+               
     finally:
-        driver.quit()
+        driver.quit()    
--- SSD_inserter.py
+++ SSD_inserter.py
@@ -1,13 +1,11 @@
 import mysql.connector
-from mysql.connector import connect, Error
+
 import configparser as cf
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.common.keys import Keys
 from time import sleep
 from random import randint
-
+from selenium.webdriver.common.by import By
+from datetime import datetime
 
 #function to check if an entry has already been inserted into the table
 def checkIfExists(cursor, table_name, column_name, value):
@@ -40,10 +38,10 @@ def parseSsdPage(url, cursor):
     column_name = "model"
     
     driver.get(url)
-    sleep(randint(5, 10))
+    sleep(30)
     #get all the information on the page into the sections variable
-    sections = driver.find_elements_by_css_selector("section.details")
-    model = driver.find_element_by_class_name("drivename").text
+    sections = driver.find_elements(By.CSS_SELECTOR, "section.details")
+    model = driver.find_element(By.CLASS_NAME, "drivename").text
     #check if this model is already in the sql table
     if(checkIfExists(cursor, table_name, column_name, model) == True):
         print(f"{model} already in table")
@@ -51,71 +49,71 @@ def parseSsdPage(url, cursor):
 
     #loop through the information and put all relevant specs into their variable
     for section in sections:
-        h1_element = section.find_element_by_css_selector("h1")
+        h1_element = section.find_element(By.CSS_SELECTOR, "h1")
         
         if(h1_element.text == "Solid-State-Drive"):
-            rows = section.find_elements_by_css_selector("tr")
+            rows = section.find_elements(By.CSS_SELECTOR, "tr")
 
             for row in rows:
-                header = row.find_element_by_css_selector("th")
+                header = row.find_element(By.CSS_SELECTOR, "th")
                 if(header.text == "Capacity:"):
-                    overall_capacity = row.find_element_by_css_selector("td").text
+                    overall_capacity = row.find_element(By.CSS_SELECTOR, "td").text
         
         elif(h1_element.text == "NAND Flash"):
-            rows = section.find_elements_by_css_selector("tr")
+            rows = section.find_elements(By.CSS_SELECTOR, "tr")
 
             for row in rows:
-                header = row.find_element_by_css_selector("th")
+                header = row.find_element(By.CSS_SELECTOR, "th")
                 if(header.text == "Type:"):
-                    nand_type = row.find_element_by_css_selector("td").text
+                    nand_type = row.find_element(By.CSS_SELECTOR, "td").text
                 elif(header.text == "Technology:"):
-                    nand_technology = row.find_element_by_css_selector("td").text
+                    nand_technology = row.find_element(By.CSS_SELECTOR, "td").text
                 elif(header.text == "Capacity:"):
-                    nand_capacity = row.find_element_by_css_selector("td").text
+                    nand_capacity = row.find_element(By.CSS_SELECTOR, "td").text
         
         elif(h1_element.text == "Physical"):
-            rows = section.find_elements_by_css_selector("tr")
+            rows = section.find_elements(By.CSS_SELECTOR, "tr")
 
             for row in rows:
-                header = row.find_element_by_css_selector("th")
+                header = row.find_element(By.CSS_SELECTOR, "th")
                 if(header.text == "Form Factor:"):
-                    form_factor = row.find_element_by_css_selector("td").text
+                    form_factor = row.find_element(By.CSS_SELECTOR, "td").text
                 elif(header.text == "Interface:"):
-                    interface = row.find_element_by_css_selector("td").text
+                    interface = row.find_element(By.CSS_SELECTOR, "td").text
                 elif(header.text == "Protocol:"):
-                    protocol = row.find_element_by_css_selector("td").text
+                    protocol = row.find_element(By.CSS_SELECTOR, "td").text
 
         elif(h1_element.text == "Controller"):
-            rows = section.find_elements_by_css_selector("tr")
+            rows = section.find_elements(By.CSS_SELECTOR, "tr")
 
             for row in rows:
-                header = row.find_element_by_css_selector("th") 
+                header = row.find_element(By.CSS_SELECTOR, "th") 
                 if(header.text == "Architecture:"):
-                    controller = row.find_element_by_css_selector("td").text
+                    controller = row.find_element(By.CSS_SELECTOR, "td").text
 
         elif(h1_element.text == "DRAM Cache"):
-            rows = section.find_elements_by_css_selector("tr")
+            rows = section.find_elements(By.CSS_SELECTOR, "tr")
 
             for row in rows:
-                header = row.find_element_by_css_selector("th")   
+                header = row.find_element(By.CSS_SELECTOR, "th")   
                 if(header.text == "Capacity:"):
-                    dram = row.find_element_by_css_selector("td").text
+                    dram = row.find_element(By.CSS_SELECTOR, "td").text
 
         elif(h1_element.text == "Performance"):
-            rows = section.find_elements_by_css_selector("tr")
+            rows = section.find_elements(By.CSS_SELECTOR, "tr")
 
             for row in rows:
-                header = row.find_element_by_css_selector("th")   
+                header = row.find_element(By.CSS_SELECTOR, "th")   
                 if(header.text == "Sequential Read:"):
-                    read_speed = row.find_element_by_css_selector("td").text
+                    read_speed = row.find_element(By.CSS_SELECTOR, "td").text
                 elif(header.text == "Sequential Write:"):
-                    write_speed = row.find_element_by_css_selector("td").text
+                    write_speed = row.find_element(By.CSS_SELECTOR, "td").text
                 elif(header.text == "Random Read:"):
-                    random_read = row.find_element_by_css_selector("td").text
+                    random_read = row.find_element(By.CSS_SELECTOR, "td").text
                 elif(header.text == "Random Write:"):
-                    random_write = row.find_element_by_css_selector("td").text
+                    random_write = row.find_element(By.CSS_SELECTOR, "td").text
                 elif(header.text == "Endurance:"):
-                    endurance = row.find_element_by_css_selector("td").text
+                    endurance = row.find_element(By.CSS_SELECTOR, "td").text
     #insert into the table
     query = "INSERT INTO ssd (capacity, interface, read_speed, write_speed, endurance, dram, model, protocol, form_factor, controller, nand_type, nand_capacity, nand_technology, random_read, random_write)  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     values = (overall_capacity, interface, read_speed, write_speed, endurance, dram, model, protocol, form_factor, controller, nand_type, nand_capacity, nand_technology, random_read, random_write)
@@ -128,61 +126,61 @@ def parseSsdPage(url, cursor):
 # endurance, /dram, model, /protocol, /form_factor, /controller(architecture)
 #/nand_type, /nand_capacity, /nand_technology,
 
-
-options = webdriver.ChromeOptions()
-options.binary_location = r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe'
-
-# Create the ChromeDriver instance for Brave
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install(), options=options)
+driver = webdriver.Chrome()
 
 # Open a website
 driver.get('https://www.techpowerup.com/ssd-specs/')
+sleep(15)
 # find the search bar
-search_bar = driver.find_element_by_css_selector(".js-search-input.search-input")
+search_bar = driver.find_element(By.CSS_SELECTOR, ".js-search-input.search-input")
 #the approach: type two letters into search bar (aa, ab, ac, etc) and then visit each entry and insert based on info from page
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-counter = 0
-try:
-    config = cf.ConfigParser()
-    config.read('config.ini')
-    db_host = config.get('Database', 'HOST') 
-    db_user = config.get('Database', 'USER')
-    db_password = config.get('Database', 'PASSWORD')
-    db_name = config.get('Database', 'DATABASE')
-    conn = mysql.connector.connect(
-        host = db_host,
-        user = db_user,
-        password = db_password,
-        database = db_name
-    )
-    cursor = conn.cursor()
-    table_name = "ssd"
-    column_name = "model"
-    value = "temp"
-    for i in range(len(alphabet)):
-        for j in range(len(alphabet)):
-            if(counter > 453):
-                #we need to reload the page and relocate the search bar each time because we will be loading new pages
-                driver.get('https://www.techpowerup.com/ssd-specs/')
-                search_bar = driver.find_element_by_css_selector(".js-search-input.search-input")
-                search_bar.send_keys(f"{alphabet[i]}{alphabet[j]}")
-                sleep(5)
-                ssd_table = driver.find_elements_by_css_selector(".drive-title")
-                link_attributes = []
-                for item in ssd_table:
-                    a_elements = item.find_elements_by_tag_name('a')
-                    for result in a_elements:
-                        if result.text.strip():
-                            href = result.get_attribute("href")
-                            text = result.text
-                            link_attributes.append({"href": href, "text": text})
+while(True):
+    counter = 0
+    try:
+        config = cf.ConfigParser()
+        config.read('config.ini')
+        db_host = config.get('Database', 'HOST') 
+        db_user = config.get('Database', 'USER')
+        db_password = config.get('Database', 'PASSWORD')
+        db_name = config.get('Database', 'DATABASE')
+        conn = mysql.connector.connect(
+            host = db_host,
+            user = db_user,
+            password = db_password,
+            database = db_name
+        )
+        cursor = conn.cursor()
+        table_name = "ssd"
+        column_name = "model"
+        value = "temp"
+        for i in range(len(alphabet)):
+            for j in range(len(alphabet)):
+                if(counter > 476):
+                    #we need to reload the page and relocate the search bar each time because we will be loading new pages
+                    driver.get('https://www.techpowerup.com/ssd-specs/')
+                    search_bar = driver.find_element(By.CSS_SELECTOR, ".js-search-input.search-input")
+                    search_bar.send_keys(f"{alphabet[i]}{alphabet[j]}")
+                    sleep(5)
+                    ssd_table = driver.find_elements(By.CSS_SELECTOR, ".drive-title")
+                    link_attributes = []
+                    for item in ssd_table:
+                        a_elements = item.find_elements(By.TAG_NAME, 'a')
+                        for result in a_elements:
+                            if result.text.strip():
+                                href = result.get_attribute("href")
+                                text = result.text
+                                link_attributes.append({"href": href, "text": text})
 
-                for item in link_attributes:
-                    parseSsdPage(item["href"], cursor)
-            counter += 1
-            print(f"counter is {counter}")
-                        
-     
-finally:
-    driver.quit()
+                    for item in link_attributes:
+                        parseSsdPage(item["href"], cursor)
+                counter += 1
+                print(f"counter is {counter}")
+ 
+    except:
+        now = datetime.now()
+        print(f"Error at {now.strftime('%H:%M:%S')}. Waiting for 20 minutes")
+        sleep(1200)                        
+    
+    
